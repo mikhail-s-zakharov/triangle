0c40eab5e8650f41ca2e9625d1dfc26d3d5cebb0
Mikhail Zakharov <mzakharov@setronica.com>
2016-10-11 13:42:58 +0700

    Project initialized

---
+++ src/test/java/com/setronica/mzakharov/trianglechallenge/test/TrianglesTest.java
@@ -0,0 +1,15 @@
+package com.setronica.mzakharov.trianglechallenge.test;
+
+import org.junit.Test;
+
+/**
+ * Created by mzakharov on 11.10.16.
+ */
+public class TrianglesTest {
+    @Test
+    public void sidesFormTriangle(){
+
+    }
+
+
+}
